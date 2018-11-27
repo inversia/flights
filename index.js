0f6d9fe66a272cf25a98d1201e1f665c2e8e4088
inversia <mrs.shpritz@gmail.com>
2018-11-28 02:22:04 +0300

    font add

--- index.js
+++ index.js
@@ -4,5 +4,14 @@ const $$ = document.querySelectorAll.bind(document)
 
 document.addEventListener ('DOMContentLoaded', () => {  
 
+    const buttons = $$('.buttons button')
+    
+    for (const button of buttons){
+        
+        button.onclick = () => {
 
+            alert('ХУЯССЕ!')
+        }
+    }
 })
+
